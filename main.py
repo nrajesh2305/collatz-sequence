fa57a2bde14d31ba668c3537bdac5857a929c88e
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-10 15:08:05 -0800

    Finished the project. Enjoy using Collatz Sequence! :)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+from collatz import *
 
-# while num <= 0:
-#     print("Please input a valid starting number.")
-#     num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+printIntro()
+
+num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+
+while num <= 0:
+    print("Please input a valid starting number.")
+    num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+
+print("\nThe sequence is: " + performFormula(num))
