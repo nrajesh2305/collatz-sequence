7cc89a5bdd77fec65f1aeebb6dfc29d336267a8f
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-10 14:54:57 -0800

    Finished creating the logic needed to check if the number inputted is a valid choice.

--- collatz.py
+++ collatz.py
@@ -1,4 +1,4 @@
-
+# No imports are necessary for this file.
 
 # Replace with the portfolio website when done.
 website_url = ""
@@ -6,4 +6,9 @@ website_url = ""
 def printIntro():
     print("\nCollatz Sequence, or the 3n + 1 Problem\nBy Nithin Rajesh " + website_url + "\n\nThe Collatz sequence is a sequence of numbers produced from a starting number n, following three rules:\n\n1) If n is even, the next number is n / 2.\n2) If n is odd, the next number n is n * 3 + 1.\n3) If n is 1, stop. Otherwise, repeat.\n\nIt is generally thought, but so far not mathematically proven, that every starting number eventually terminates at 1.\n")
 
-printIntro()
+# num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+
+# while num <= 0:
+#     print("Please input a valid starting number.")
+#     num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+
