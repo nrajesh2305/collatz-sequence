187730243603d15e73f5fe9dd8ae3809f1550599
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-10 15:04:33 -0800

    Finished the collatz.py file. Works 100%.

--- collatz.py
+++ collatz.py
@@ -6,9 +6,15 @@ website_url = ""
 def printIntro():
     print("\nCollatz Sequence, or the 3n + 1 Problem\nBy Nithin Rajesh " + website_url + "\n\nThe Collatz sequence is a sequence of numbers produced from a starting number n, following three rules:\n\n1) If n is even, the next number is n / 2.\n2) If n is odd, the next number n is n * 3 + 1.\n3) If n is 1, stop. Otherwise, repeat.\n\nIt is generally thought, but so far not mathematically proven, that every starting number eventually terminates at 1.\n")
 
-# num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
-
-# while num <= 0:
-#     print("Please input a valid starting number.")
-#     num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
-
+def performFormula(number):
+    fullSequence = ""
+    while True:
+        if(number == 1):
+            fullSequence += str(1)
+            break
+        fullSequence += str(number) + " "
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+    return fullSequence
---
+++ main.py
@@ -0,0 +1,5 @@
+# num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
+
+# while num <= 0:
+#     print("Please input a valid starting number.")
+#     num = int(input("Enter a starting number (greater than 0) or QUIT:\n"))
