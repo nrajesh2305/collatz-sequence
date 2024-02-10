4b3e8a8ad58b2bd229fa7a2bdf194aa5584535cd
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2024-02-10 11:34:24 -0800

    Finished making a printIntro function.

---
+++ collatz.py
@@ -0,0 +1,9 @@
+
+
+# Replace with the portfolio website when done.
+website_url = ""
+
+def printIntro():
+    print("\nCollatz Sequence, or the 3n + 1 Problem\nBy Nithin Rajesh " + website_url + "\n\nThe Collatz sequence is a sequence of numbers produced from a starting number n, following three rules:\n\n1) If n is even, the next number is n / 2.\n2) If n is odd, the next number n is n * 3 + 1.\n3) If n is 1, stop. Otherwise, repeat.\n\nIt is generally thought, but so far not mathematically proven, that every starting number eventually terminates at 1.\n")
+
+printIntro()
